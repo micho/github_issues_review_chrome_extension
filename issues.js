3591a0806ef59cc2d0a8f40290d84505379f724f
Pablo Villalba <pablo@teambox.com>
2013-03-25 12:06:29 +0100

    JS to track votes and display them

---
+++ issues.js
@@ -0,0 +1,61 @@
+/**
+ * Remember PRs I've seen and display them in the issues view
+ */
+
+$(function () {
+
+  // ["/teambox/Teambox-hosted/issues", "teambox", "Teambox-hosted"]
+  var matches = location.pathname.match(/\/(.*?)\/(.*?)\/issues/)
+    , my_username = $("#user-links a.name").text().trim();
+
+  // If this is not the Issues view, GTFO
+  if (!matches) {
+    return;
+  }
+
+  function annotateIssues() {
+    // For each issue, add an approval count
+    $(".js-issues-list").find("a.js-navigation-open").each(function () {
+      var $a = $(this)
+        , href = $a.attr('href');
+
+      // Don't process twice
+      if ($a.hasClass('already-annotated')) {
+        return;
+      }
+      $a.addClass('already-annotated');
+
+      // Get data from chrome.storage (which is populated by pull.js when viewing a PR)
+      chrome.storage.sync.get(href, function (data) {
+        var voters = data[href] || []
+          , other_voters = _(voters).without(my_username);
+
+        // Add my own thumb if I already approved this PR
+        if (voters.indexOf(my_username) !== -1) {
+          $a.parents('.info-wrapper').append('<p style="position: absolute; right: -25px; top: 10px; "><img class="emoji" title=":+1:" alt=":+1:" src="https://a248.e.akamai.net/assets.github.com/images/icons/emoji/+1.png" height="20" width="20" align="absmiddle"></p>');
+        }
+
+        // Show the count of other people's votes
+        if (other_voters.length) {
+          var $list_item = $a.parents('.list-browser-item');
+
+          // If more than 2 people voted it, color it green and push it to the bottom
+          if (other_voters.length >= 2) {
+            $list_item.css({ 'background-color': 'rgba(0, 255, 0, 0.1)' });
+            $list_item.parents('tbody').append($list_item);
+          }
+
+          // Add number of votes
+          $a.parents('.info-wrapper').append('<p style="position: absolute; right: -57px; top: 10px; font-size: 18px">+ ' + other_voters.length + '</p>');
+        }
+      });
+    });
+  }
+
+  annotateIssues();
+  // Dirty, but it works: update when changing Issues filters
+  setInterval(function () {
+    annotateIssues();
+  }, 500);
+
+});
---
+++ pull.js
@@ -0,0 +1,35 @@
+/**
+ * When in the PR view, record if we see a :+1: from my user
+ */
+
+$(function () {
+
+  // ["/teambox/Teambox-hosted/pull/1155", "teambox", "Teambox-hosted", "1155"]
+  var matches = location.pathname.match(/\/(.*?)\/(.*?)\/pull\/(\d+)/)
+    , pr_key = location.pathname;
+
+  // Not a PR URL? GTFO
+  if (!matches) {
+    return;
+  }
+
+  // Count votes by others
+  function usersWhoVoted() {
+    var voted_by = []
+      , my_username = $("#user-links a.name").text().trim();
+
+    $(".comment-header-author").each(function () {
+      if ($(this).parents(".discussion-bubble-inner").find("img[title=':+1:']").length) {
+        voted_by.push($(this).text());
+      }
+    });
+
+    return voted_by;
+  }
+
+  // Persist the array of users who voted
+  var hash = {};
+  hash[pr_key] = usersWhoVoted();
+  chrome.storage.sync.set(hash);
+});
+
