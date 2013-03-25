d7de4aceaebfac3155735094c061488b0fd0bd25
Pablo Villalba <pablo@teambox.com>
2013-03-25 13:49:47 +0100

    Write only when data is different, respect quota

--- pull.js
+++ pull.js
@@ -4,7 +4,7 @@
  * When in the PR view, record if we see a :+1: from my user
  */
 
-$(function () {
+(function () {
 
   // ["/teambox/Teambox-hosted/pull/1155", "teambox", "Teambox-hosted", "1155"]
   var matches = location.pathname.match(/\/(.*?)\/(.*?)\/pull\/(\d+)/)
@@ -29,26 +29,33 @@ $(function () {
     return _(voted_by).uniq();
   }
 
-  // Persist the array of users who voted
+  // Persist the array of users who voted (only if different, to avoid quota limits)
   function saveVotesCount() {
     var hash = {};
     hash[pr_key] = usersWhoVoted();
-    chrome.storage.sync.set(hash);
+    chrome.storage.sync.get(pr_key, function (stored) {
+      console.log(hash[pr_key], stored[pr_key], hash[pr_key] !== stored[pr_key]);
+      if (hash[pr_key] !== stored[pr_key]) {
+        chrome.storage.sync.set(hash);
+      }
+    });
   }
 
-  saveVotesCount();
-
-  // Dirty, but it works: update after submitting a comment
-  $("form").submit(function () {
-    setTimeout(function () {
-      saveVotesCount();
-    }, 2000);
-  });
-
-  // Save when closing window
-  $(window).unload(function () {
+  $(function () {
     saveVotesCount();
+
+    // Dirty, but it works: update after submitting a comment
+    $("form").submit(function () {
+      setTimeout(function () {
+        saveVotesCount();
+      }, 2000);
+    });
+
+    // Save when closing window
+    $(window).unload(function () {
+      saveVotesCount();
+    });
   });
 
-});
+}());
 
